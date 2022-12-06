8d6f87525581bac748a804dbb6a4fe982cf479cd
Philip Dimaano <pdimaano7@gmail.com>
2022-12-05 17:23:13 -0800

    Changed strict in tsconfig to true

--- boxes/src/index.tsx
+++ boxes/src/index.tsx
@@ -5,11 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
