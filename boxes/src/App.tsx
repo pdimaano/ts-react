c17c436faa9f26926aa75a2a489700aeba6706cf
Philip Dimaano <pdimaano7@gmail.com>
2022-12-05 16:49:30 -0800

    Added Typescript to Boxes component

--- boxes/src/App.tsx
+++ boxes/src/App.tsx
@@ -1,5 +1,14 @@
 import BoxList from "./BoxList";
 
+/** Color box maker application
+ *
+ *  Props: None
+ *
+ *  State: None
+ *
+ *  App -> BoxList
+ */
+
 function App() {
   return (
     <div>
--- boxes/src/Box.tsx
+++ boxes/src/Box.tsx
@@ -10,7 +10,15 @@
  * BoxList -> Box
  */
 
-function Box({ id, width = 5, height = 5, backgroundColor, remove }) {
+interface BoxPropsInterface {
+  id: number;
+  width: number;
+  height: number;
+  backgroundColor: string;
+  remove: Function;
+}
+
+function Box({ id, width = 5, height = 5, backgroundColor, remove }: BoxPropsInterface) {
 
   /** Remove a box. */
   function handleRemove() {
