06ba6738576d182cbf11b700f6e75a088901ae73
Philip Dimaano <pdimaano7@gmail.com>
2024-01-16 12:44:29 -0800

    updated for react strict mode

--- boxes/src/index.tsx
+++ boxes/src/index.tsx
@@ -7,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
