63e16ea1016d7527967a05ebd7b0b9b7b8f3f62e
Philip Dimaano <pdimaano7@gmail.com>
2024-01-22 12:03:08 -0800

    initial test for rendering

--- boxes/src/Box.test.tsx
+++ boxes/src/Box.test.tsx
@@ -1,3 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Box from "./Box";
+
+const mock = () => { };
+
+it("renders without crashing", function () {
+    render(<Box id="1" width={10} height={10} backgroundColor="pink" remove={mock} />);
+});
