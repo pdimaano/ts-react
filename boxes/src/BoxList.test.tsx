c2296a962be3b23e056c84253d020e98fda72ff6
Philip Dimaano <pdimaano7@gmail.com>
2024-01-24 11:44:35 -0800

    initial variables for testing box values

--- boxes/src/BoxList.test.tsx
+++ boxes/src/BoxList.test.tsx
@@ -1,3 +1,10 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import BoxList from "./BoxList";
+
+/** Adding a box for testing */
+function addBox(container: HTMLElement, height = 2, width = 3, color = "peachpuff") {
+    const heightInput = container.querySelector("[name=height]");
+    const widthInput = container.querySelector("[name=width]");
+    const backgroundInput = container.querySelector("[name=backgroundColor]");
+}
