81c5119374b5bf64729e8a16106132e7ce221fc2
Philip Dimaano <pdimaano7@gmail.com>
2024-02-01 13:41:06 -0800

    test for initial form snapshot

--- boxes/src/NewBoxForm.test.tsx
+++ boxes/src/NewBoxForm.test.tsx
@@ -4,3 +4,8 @@ import NewBoxForm from "./NewBoxForm";
 it("renders without crashing", function () {
     render(<NewBoxForm createBox={jest.fn} />);
 });
+
+it("matches initial form snapshot", function () {
+    const { asFragment } = render(<NewBoxForm createBox={jest.fn} />);
+    expect(asFragment()).toMatchSnapshot();
+});
