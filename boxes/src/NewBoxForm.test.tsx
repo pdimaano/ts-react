a6f2cfb15745830a1ea31f10cfd75b690f743978
Philip Dimaano <pdimaano7@gmail.com>
2024-02-01 13:38:48 -0800

    test for initial rendering

--- boxes/src/NewBoxForm.test.tsx
+++ boxes/src/NewBoxForm.test.tsx
@@ -1,2 +1,6 @@
 import { render, fireEvent } from "@testing-library/react";
 import NewBoxForm from "./NewBoxForm";
+
+it("renders without crashing", function () {
+    render(<NewBoxForm createBox={jest.fn} />);
+});
