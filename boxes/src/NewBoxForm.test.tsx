66391031519cf418e9c3272d9abef10d701320de
Philip Dimaano <pdimaano7@gmail.com>
2024-02-05 14:22:52 -0800

    snapshot test after entering data

--- boxes/src/NewBoxForm.test.tsx
+++ boxes/src/NewBoxForm.test.tsx
@@ -9,3 +9,17 @@ it("matches initial form snapshot", function () {
     const { asFragment } = render(<NewBoxForm createBox={jest.fn} />);
     expect(asFragment()).toMatchSnapshot();
 });
+
+it("matches data-entered snapshot", function () {
+    const { container } = render(<NewBoxForm createBox={jest.fn} />);
+
+    const heightInput = container.querySelector("[name=height]");
+    const widthInput = container.querySelector("[name=width]");
+    const bgColorInput = container.querySelector("[name=backgroundColor]");
+
+    fireEvent.input(heightInput!, { target: { value: "7" } });
+    fireEvent.input(widthInput!, { target: { value: "8" } });
+    fireEvent.input(bgColorInput!, { target: { value: "firebrick" } });
+
+    expect(container).toMatchSnapshot();
+})
