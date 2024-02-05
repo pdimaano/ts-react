9192b474b2b0d3daf8c51b148cc6098d8e0728ea
Philip Dimaano <pdimaano7@gmail.com>
2024-02-05 14:27:09 -0800

    test for form submission

--- boxes/src/NewBoxForm.test.tsx
+++ boxes/src/NewBoxForm.test.tsx
@@ -22,4 +22,26 @@ it("matches data-entered snapshot", function () {
     fireEvent.input(bgColorInput!, { target: { value: "firebrick" } });
 
     expect(container).toMatchSnapshot();
-})
+});
+
+it("submitting form works", function () {
+    const mockCreateBox = jest.fn();
+    const { container } = render(<NewBoxForm createBox={mockCreateBox} />);
+
+    const heightInput = container.querySelector("[name=height]");
+    const widthInput = container.querySelector("[name=width]");
+    const bgColorInput = container.querySelector("[name=backgroundColor]");
+
+    fireEvent.input(heightInput!, { target: { value: "7" } });
+    fireEvent.input(widthInput!, { target: { value: "8" } });
+    fireEvent.input(bgColorInput!, { target: { value: "firebrick" } });
+
+    expect(mockCreateBox).toHaveBeenCalledTimes(0);
+
+    fireEvent.click(container.querySelector(".NewBoxForm-addBtn")!)
+    expect(mockCreateBox).toHaveBeenCalledTimes(1);
+
+    // all form inputs should be empty
+    expect(container.querySelectorAll("input[value='']")).toHaveLength(3);
+
+});
